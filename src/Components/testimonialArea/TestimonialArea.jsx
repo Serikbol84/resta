4e4767a3f8ec174faf7c57b63e3c0f5e2e777f7c
Serikbol <sh.serikbol@gmail.com>
2024-10-10 10:59:37 +0500

    finished testimonial sliders part

--- src/Components/testimonialArea/TestimonialArea.jsx
+++ src/Components/testimonialArea/TestimonialArea.jsx
@@ -1,8 +1,25 @@
 
-import author from "../../img/testimonial/author.png"
+
 import "./style.css"
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { slides } from "./testimonialSliderData";
+
+
 
 const TestimonialArea = () => {
+
+    const settings = {
+        dots: true, // Точки показывать снизу которые
+        infinite: false, // бесконечная прокрутка (откл)
+        speed: 500,
+        slidesToShow: 2, // Показывать 2 слайда
+        slidesToScroll: 2, // Прокручивать 2 слайда за раз
+        // rows: 1, // Один ряд слайдов
+        // arrows: false
+    };
+
     return (
         <div className="tsetimonial-area overlay">
             <div className="container">
@@ -12,30 +29,30 @@ const TestimonialArea = () => {
                 </div>
 
                 <div className="testimonial-active">
-                    <div className="single-carousel">
-                        <div className="single_testmonial">
-                            <div class="author_opinion">
-                                <p>“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor
-                                    sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed
-                                    neque.</p>
-                            </div>
-                            <div className="testimonial_author">
-                                <div class="thumb">
-                                    <img src={author} alt="Author pic" />
+                    <Slider {...settings}>
+                        {slides.map((slide) => (
+                            <div className="single-carousel" key={slide.id}>
+                                <div className="single_testmonial">
+                                    <div class="author_opinion">
+                                        <p>{slide.authorsOpinion}</p>
+                                    </div>
+                                    <div className="testimonial_author">
+                                        <div class="thumb">
+                                            <img src={slide.src} alt="Authors pic" />~
+                                        </div>
+                                        <div className="name">
+                                            <h3>{slide.authorsName}</h3>
+                                            <ul className="icon-star-row">
+                                                {Array(5).fill(null).map((_, index) => ( // создаём массив из 5 элементов (пустых значений)
+                                                    <li className="icon-star" key={index}></li>
+                                                ))}; 
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="name">
-                                    <h3>Robert Thomson</h3>
-                                    <ul className="icon-star-row">
-                                        <li className="icon-star"></li>
-                                        <li className="icon-star"></li>
-                                        <li className="icon-star"></li>
-                                        <li className="icon-star"></li>
-                                        <li className="icon-star"></li>
-                                    </ul>
-                                </div>
                             </div>
-                        </div>
-                    </div>
+                        ))}
+                    </Slider>
                 </div>
             </div>
         </div>
---
+++ src/Components/testimonialArea/testimonialSliderData.jsx
@@ -0,0 +1,44 @@
+
+import author1 from "../../img/testimonial/author.png"
+import author2 from "../../img/testimonial/author2.png"
+
+const slides = [
+    {
+        id: 'slide-1', 
+        authorsOpinion: '“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed neque.',
+        src: author1,
+        authorsName: 'Robert Thomson'
+    },
+    {
+        id: 'slide-2', 
+        authorsOpinion: '“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed neque.',
+        src: author2,
+        authorsName: 'Kristiana Chouhan'
+    },
+    {
+        id: 'slide-3', 
+        authorsOpinion: '“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed neque.',
+        src: author1,
+        authorsName: 'Albert Minigun'
+    },
+    {
+        id: 'slide-4', 
+        authorsOpinion: '“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed neque.',
+        src: author2,
+        authorsName: 'Karolina Chouha'
+    },
+    {
+        id: 'slide-5', 
+        authorsOpinion: '“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed neque.',
+        src: author1,
+        authorsName: 'Roberto Charlos'
+    },
+    {
+        id: 'slide-6', 
+        authorsOpinion: '“Donec imperdiet congue orci consequat mattis. Donec rutrum porttitor sollicitudin. Pellentesque id dolor tempor sapien feugiat ultrices nec sed neque.',
+        src: author2,
+        authorsName: 'Kristina Arbakaite'
+    }
+];
+
+export {slides};
